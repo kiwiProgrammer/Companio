74344fc3a58eb726be45519a48a7b98943b23c5b
porporboy <yoyozwassup@hotmail.com>
2016-09-25 11:06:54 -0700

    change arrow coords

--- app/src/main/java/com/companio/vr/AugmentedRealityRenderer.java
+++ app/src/main/java/com/companio/vr/AugmentedRealityRenderer.java
@@ -91,7 +91,7 @@ public class AugmentedRealityRenderer extends RajawaliRenderer {
         }
         getCurrentScene().addChildAt(backgroundQuad, 0);
 
-        renderObjects(6, 0);
+        renderObjects(-4, 8);
         // Create sphere with moon texture.
 
         Material moonMaterial = new Material();
